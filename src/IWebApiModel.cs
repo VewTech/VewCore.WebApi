fb8324af14ad89518a0158caf9e8e916254142b8
Josep Sendra Serra <je.sendra@vewtech.net>
2023-10-28 16:01:34 +0200

    Added IWebApiHelper functionality to CrudController

--- src/IWebApiModel.cs
+++ src/IWebApiModel.cs
@@ -6,7 +6,6 @@ public interface IWebApiModel
     public Guid? CreatedBy { get; set; }
     public DateTime CreatedTimestamp { get; set; }
     public Guid? UpdatedBy { get; set; }
-    public DateTime UpdatedTimestamp { get; set; }
-    public bool Disabled { get; set; }
+    public DateTime? UpdatedTimestamp { get; set; }
     public DateTime? DisabledTimestamp { get; set; }
 }

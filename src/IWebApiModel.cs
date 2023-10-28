525acaf637454fd5e7c96171ed96267ca20acd6a
Josep Sendra Serra <je.sendra@vewtech.net>
2023-10-28 13:57:13 +0200

    Made UpdatedBy nullable

--- src/IWebApiModel.cs
+++ src/IWebApiModel.cs
@@ -5,7 +5,7 @@ public interface IWebApiModel
     public Guid Id { get; set; }
     public Guid? CreatedBy { get; set; }
     public DateTime CreatedTimestamp { get; set; }
-    public Guid UpdatedBy { get; set; }
+    public Guid? UpdatedBy { get; set; }
     public DateTime UpdatedTimestamp { get; set; }
     public bool Disabled { get; set; }
     public DateTime? DisabledTimestamp { get; set; }

1f285632c40a538e1c1ea3ea53eec43a1cd9d665
Josep Sendra Serra <je.sendra@vewtech.net>
2023-11-09 11:44:01 +0100

    Deleted unnecessary test

--- WebApiTest/DataContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using VewTech.VewCore.WebApi.WebApiTest.Models;
-
-namespace VewTech.VewCore.WebApi.WebApiTest;
-
-public class DataContext(DbContextOptions<DataContext> dbContextOptions) : DbContext(dbContextOptions)
-{
-    public DbSet<TestModel> TestModels { get; set; }
-}
